6fbd5405f4f608138f079024181b4c1af17115af
chenyue <285653184@qq.com>
2022-06-18 22:46:36 +0800

    add bundle

--- docs/.vuepress/config-page.ts
+++ docs/.vuepress/config-page.ts
@@ -3,12 +3,13 @@
  * @Author: cy
  * @Date: 2022-05-09 20:59:45
  * @LastEditors: cy
- * @LastEditTime: 2022-06-18 22:35:15
+ * @LastEditTime: 2022-06-18 22:45:22
  */
 import { defineUserConfig } from 'vuepress'
 import userConfig from './config'
+import { webpackBundler } from "@vuepress/bundler-webpack"
 
 export default defineUserConfig(Object.assign({}, userConfig, {
-    bundler: undefined,
+    bundler: webpackBundler({}),
     base: '/learning-notes/'
 }))
